b9d22e8fc8d9b3f65f8879c25d0e724ad13591fd
Kevin Borling <kborling@protonmail.com>
2022-12-26 14:46:17 -0500

    Initial Commit
    Testing with the 6.1-2 release.

---
+++ asahi-config.scm
@@ -0,0 +1,29 @@
+(use-modules (guix packages)
+             (guix download)
+             (guix build-system gnu))
+
+(define-public kernel-asahi
+  (package
+    (name "kernel-asahi")
+    (version "asahi-6.1-2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/AsahiLinux/linux/releases/tag/"
+                    version ".tar.gz"))
+              (sha256
+                (base32
+                  "a308564a13dd89144200a1058ad34ff8e26b0822e143fbeda0b3769a5fa1c60e"))))
+    (build-system gnu-build-system)
+    (arguments
+      `(#:phases
+        (modify-phases %standard-phases
+          (delete 'configure)
+          (add-before 'build 'unpack-sources
+            (lambda _
+              (chdir version
+                (system* "make" "defconfig")))))))
+    (native-inputs `(("bash" ,bash)))
+    (home-page "https://github.com/AsahiLinux/kernel")
+    (synopsis "The Asahi Linux kernel")
+    (description "The Asahi Linux kernel is a Linux kernel distribution based on the upstream Linux kernel, with additional patches and modifications for better support on certain devices.")))
